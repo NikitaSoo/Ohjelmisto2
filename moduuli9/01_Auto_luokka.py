db858a620343fb98333c6571e14c39b58a5da205
Nikita <nikitasoo196@gmail.com>
2025-03-19 14:31:07 +0200

    Uudistus

--- moduuli9/01_Auto_luokka.py
+++ moduuli9/01_Auto_luokka.py
@@ -1,14 +1,14 @@
 class Auto:
-    def __init__(self, tunnus, huippunopeus):
-        self.tunnus = tunnus
+    def __init__(self, rtunnus, huippunopeus):
+        self.rtunnus = rtunnus
         self.huippunopeus = huippunopeus
         self.nykynopeus = 0
         self.matka = 0
 
 auto = Auto("ABC-123", 142)
 
-print(f"Auton rekisteritunnus: {auto.tunnus}")
+print(f"Auton rekisterinumero: {auto.rtunnus}")
 print(f"Auton huippunopeus: {auto.huippunopeus} km/h")
-print(f"Auton tämänhetkinen nopeus: {auto.nykynopeus}")
+print(f"Auton nykynopeus: {auto.nykynopeus}")
 print(f"Auton kuljettu matka: {auto.matka}")
 
